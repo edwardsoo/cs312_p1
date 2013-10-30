59eaf6a5100da499e78ab9a7efd11acac3c0090c
Edward Soo <paktiksoo@gmail.com>
2013-10-29 20:57:09 -0700

    init commit

---
+++ project1.hs
@@ -0,0 +1,9 @@
+-- Name: Edward Soo, Annie Li
+-- Student Number: 71680094, 
+
+-- Returns the best next move
+-- board: current state of the game, represented by an array of rows
+-- side: 'w' or 'b'
+-- moves: the number of moves to search ahead
+oska_b0e7 :: [String] -> Char -> Integer -> [String]
+oska_b0e7 board side moves= [""]
